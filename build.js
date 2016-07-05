3289c34fcde3979f251fc5782b7472f64fd51a81
Thomas Schorn <thomas@schorn.io>
2016-07-05 14:46:15 +0200

    [minor] handlebars layouts

--- build.js
+++ build.js
@@ -4,9 +4,21 @@ const SOURCE = 'src';
 const DESTINATION = 'dist';
 
 const metalsmith = require('metalsmith');
+const markdown = require('metalsmith-markdown');
+const layouts = require('metalsmith-layouts');
 
 metalsmith(__dirname)
+  .metadata({
+    title: 'Thomas Schorn'
+  })
   .source(SOURCE)
+  .use(markdown())
+  .use(layouts({
+    engine: 'handlebars',
+    pattern: '**/*.html',
+    default: 'default.hbs',
+    partials: 'layouts/partials',
+  }))
   .destination(DESTINATION)
   .build((error) => {
     console.log(error);
